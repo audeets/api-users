2ded5d05915df018d1ce227cce0cd046de946951
Benoit Quette <benoit.quette@gmail.com>
2024-01-28 20:27:46 +0100

    do not store more than the user.id when serializing

--- auth/passport.js
+++ auth/passport.js
@@ -1,11 +1,13 @@
+const passport = require("passport");
+
 passport.serializeUser(function (user, cb) {
-    process.nextTick(function () {
-      cb(null, { id: user.id, name: user.displayName, image: user.picture });
-    });
+  process.nextTick(function () {
+    cb(null, { id: user.id });
   });
-  
-  passport.deserializeUser(function (user, cb) {
-    process.nextTick(function () {
-      return cb(null, user);
-    });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
   });
+});

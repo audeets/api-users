93af2e3434b075008b6ced91d635b2754a8bd75b
Benoit Quette <benoit.quette@gmail.com>
2024-03-11 11:17:51 +0100

    split prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true,
+    es2021: true
   },
-  extends: ["prettier", "eslint:recommended", "plugin:import/recommended"],
-  parser: "@babel/eslint-parser",
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
-      impliedStrict: true,
+      impliedStrict: true
     },
     ecmaVersion: 12,
-    requireConfigFile: false,
+    requireConfigFile: false
   },
-  plugins: ["prettier"],
   rules: {
-    "no-undef": "off",
-    "no-param-reassign": "off",
-    "import/order": 1,
-    "import/no-cycle": "off",
-    "no-console": "off",
-    "prefer-destructuring": "off",
-    "no-shadow": "off",
-    "import/no-named-as-default": "off",
-    "import/no-extraneous-dependencies": "off",
-    "no-unused-vars": [
-      "error",
+    'no-undef': 'off',
+    'no-param-reassign': 'off',
+    'import/order': 1,
+    'import/no-cycle': 'off',
+    'no-console': 'off',
+    'prefer-destructuring': 'off',
+    'no-shadow': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-vars': [
+      'error',
       {
-        ignoreRestSiblings: false,
-      },
-    ],
-    "prettier/prettier": [
-      "warn",
-      {
-        bracketSpacing: true,
-        printWidth: 140,
-        singleQuote: true,
-        trailingComma: "none",
-        tabWidth: 2,
-        useTabs: false,
-        endOfLine: "auto",
-      },
-    ],
-  },
+        ignoreRestSiblings: false
+      }
+    ]
+  }
 };
